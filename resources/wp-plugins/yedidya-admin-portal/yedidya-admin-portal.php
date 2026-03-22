9e16fa18cf01a132ab88466f87d6dac78c4ad09b
Michael Stahl <michael.stahl@testprincipia.com>
2026-03-22 21:38:32 +0200

    Auto-save: portal changes

---
+++ resources/wp-plugins/yedidya-admin-portal/yedidya-admin-portal.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Yedidya Admin Portal
+ * Description: Server-side support for the Yedidya Admin Portal desktop app. Includes GDPR user erasure endpoint and members list shortcode. Added by Michael Stahl.
+ * Version:     1.0.0
+ * Author:      Michael Stahl
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+require_once plugin_dir_path( __FILE__ ) . 'gdpr-erase.php';
+require_once plugin_dir_path( __FILE__ ) . 'members-list-link-shortcode.php';
